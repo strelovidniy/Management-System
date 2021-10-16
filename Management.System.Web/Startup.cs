9aaf67d67db0ca7b55d11b76122c4989c3f1aea6
Reverend <roma.dan2001@gmail.com>
2021-10-16 18:17:07 +0300

    Added favicon

--- Management.System.Web/Startup.cs
+++ Management.System.Web/Startup.cs
@@ -1,8 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,10 +9,7 @@ namespace Project
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
